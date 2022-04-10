90dc28ffa6d317264b39015c56b5e2cccc27cb88
antonymburia <antonymburia10@gmail.com>
2022-04-10 22:13:50 +0300

    create navigation

--- clone/views.py
+++ clone/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Project, Profile
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout
